0744fd0a967d7db0b5f8af8d18a28d7a494a8410
enicolasgomez <enicolasgomez@gmail.com>
2023-09-08 19:43:07 -0300

    Completed wireframe transformation tool

--- processor/wireframe.py
+++ processor/wireframe.py
@@ -1,12 +1,9 @@
 import nibabel as nib
 import numpy as np
-import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
-from matplotlib.widgets import CheckButtons
-from nibabel import Nifti1Image
+import matplotlib.pyplot as plt
+from skimage import measure
 import os
-
-# Load the NIfTI file
 
 nifti_file_name = 'glm_ANOVA_Psi_cFos_rb4_z_vox_p_fstat1.nii.gz'
 
@@ -18,48 +15,54 @@ nifti_file = os.path.join(current_directory, nifti_file_name)
 img = nib.load(nifti_file)
 data = img.get_fdata()
 
-# Visualize a slice from the NIfTI file
-slice_idx_to_visualize = 120  # Change this to the slice index you want to visualize
-slice_data = data[:, :, slice_idx_to_visualize]
+# Initialize a list to store the transformed slices
+transformed_slices = []
 
-plt.figure()
-plt.imshow(slice_data, cmap='gray')
-plt.title(f'Slice {slice_idx_to_visualize + 1} Visualization')
-plt.colorbar()
+# Define a custom color transformation function to create a wireframe
+def custom_color_transform(data_slice):
+    # Find contours to identify edges
+    contours = measure.find_contours(data_slice, 0.5)  # 0.5 threshold for binary values
+    
+    # Create a black canvas
+    wireframe_slice = np.zeros_like(data_slice)
+    
+    # Draw the wireframe lines on the canvas
+    for contour in contours:
+        wireframe_slice[np.round(contour[:, 0]).astype(int), np.round(contour[:, 1]).astype(int)] = 1
+    
+    return wireframe_slice
 
-# Convert each slice to wireframes
-wireframe_slices = []
-
+# Transform each slice and store it in the list
 for slice_idx in range(data.shape[-1]):
     slice_data = data[:, :, slice_idx]
     
-    # Create a meshgrid for the wireframe
-    x, y = np.meshgrid(np.arange(slice_data.shape[0]), np.arange(slice_data.shape[1]))
-    z = np.zeros_like(x)
+    # Apply the custom color transformation to the slice
+    colorized_slice = custom_color_transform(slice_data)
     
-    # Append the wireframe slice to the list
-    wireframe_slices.append(z)
+    # Append the transformed slice to the list
+    transformed_slices.append(colorized_slice)
 
-# Stack the wireframe slices back into a 3D array
-wireframe_data = np.stack(wireframe_slices, axis=-1)
+# Stack the transformed slices back into a 3D array
+transformed_data = np.stack(transformed_slices, axis=-1)
 
-# Convert data to an appropriate data type (e.g., float32)
-wireframe_data = wireframe_data.astype(np.float32)
+# Create a new NIfTI image with the transformed data
+transformed_img = nib.Nifti1Image(transformed_data, img.affine)
 
-# Create a new NIfTI image with the wireframe data
-wireframe_img = Nifti1Image(wireframe_data, img.affine)
+# Save the transformed NIfTI image to a different file
+output_nifti_file = os.path.join(current_directory, 'output_transformed.nii.gz')
+nib.save(transformed_img, output_nifti_file)
 
-# Save the wireframe NIfTI image to a different file
-output_nifti_file = 'output_wireframes.nii.gz'
-nib.save(wireframe_img, output_nifti_file)
+# Render a certain slice at the end (change slice_idx_to_render)
+slice_idx_to_render = 120  # Change this to the slice index you want to render
+if 0 <= slice_idx_to_render < transformed_data.shape[-1]:
+    plt.figure()
+    plt.imshow(transformed_data[:, :, slice_idx_to_render], cmap='gray')  # Adjust the colormap as needed
+    plt.title(f'Wireframe of Slice {slice_idx_to_render + 1}')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.grid(False)
+    plt.show()
+else:
+    print("Invalid slice index for rendering.")
 
-# Visualize a slice from the NIfTI file
-slice_idx_to_visualize = 120  # Change this to the slice index you want to visualize
-slice_data = wireframe_data[:, :, slice_idx_to_visualize]
-
-plt.figure()
-plt.imshow(slice_data, cmap='gray')
-plt.title(f'Slice {slice_idx_to_visualize + 1} Visualization')
-plt.colorbar()
-
-print(f"Wireframes saved to {output_nifti_file}")
+print(f"Transformed slices saved to {output_nifti_file}")
