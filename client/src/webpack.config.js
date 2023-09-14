e205ec3e1c49d339b66b5f020fa01cb51bc1215f
Afonso Pinto <afonsobspinto@gmail.com>
2023-09-14 13:39:34 +0100

    CFOS-32 chore: Update webpack.config.js

--- client/src/webpack.config.js
+++ client/src/webpack.config.js
@@ -52,10 +52,6 @@ module.exports = function webpacking(envVariables) {
       {
         test: /\.ts|tsx?$/,
         loader: "awesome-typescript-loader",
-        include: [
-          path.resolve(__dirname, 'src'),
-          path.resolve(__dirname, 'node_modules/ami.js')
-        ]
       },
       {
         test: /\.(css)$/,
