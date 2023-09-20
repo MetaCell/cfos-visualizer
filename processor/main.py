78bfd4c007c1ea91564957385ee2494506a81b08
enicolasgomez <enicolasgomez@gmail.com>
2023-09-20 14:32:30 -0300

    Added selenium integration

--- processor/main.py
+++ processor/main.py
@@ -2,56 +2,72 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 import os
+import http.server
+import socketserver
+import threading
 
-# Function to download a file from a URL
-def download_file(url, destination_folder):
-    response = requests.get(url)
-    if response.status_code == 200:
-        # Extract the filename from the URL
-        filename = url.split("/")[-1]
-        file_path = os.path.join(destination_folder, filename)
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
 
-        # Save the file to the destination folder
-        with open(file_path, "wb") as file:
-            file.write(response.content)
-        print(f"File '{filename}' downloaded to '{file_path}'")
-    else:
-        print(f"Failed to download file. Status code: {response.status_code}")
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-# Function to parse the HTML page and extract the file URL
-def extract_file_url(html_path):
-    with open(html_path, "r", encoding="utf-8") as html_file:
-        soup = BeautifulSoup(html_file, "html.parser")
-        # Replace this with the actual tag and attribute containing the file URL
-        file_tag = soup.find("a", href=True)
-        if file_tag:
-            file_url = file_tag["href"]
-            return file_url
-        else:
-            return None
+server_started_event = threading.Event()
+
+def start_http_server(directory, port):
+    try:
+        os.chdir(directory)
+    except FileNotFoundError:
+        print(f"Error: The directory '{directory}' does not exist.")
+        return
+
+    handler = http.server.SimpleHTTPRequestHandler
+    with socketserver.TCPServer(("", port), handler) as httpd:
+        print(f"Serving HTTP on port {port} from {directory}...")
+        server_started_event.set()
+        httpd.serve_forever()
 
 if __name__ == "__main__":
     # Check if the number of arguments is correct
-    if len(sys.argv) != 1:
-        print("Usage: python script.py arg1 arg2 arg3")
-        sys.exit(1)
+    if len(sys.argv) != 2:
+        sys.argv.append("gubra_ano_combined_25um_boundary.nii.gz")
+        #print("Usage: python script.py arg1 arg2 arg3")
+        #sys.exit(1)
 
     file_name = sys.argv[1]
 
-    # Replace with the path to your local HTML file
-    html_file_path = "website/index.html?filename=" + file_name
-    destination_folder = "processed"
-    os.makedirs(destination_folder, exist_ok=True)
+    #start the server
+    http_file_path = "http://localhost:8888/website/index.html?file=" + file_name
+    directory = os.path.dirname(os.path.abspath(__file__))
+    website_dir = directory + "/website"
+    download_dir = directory + "/process"
+    port = 8888
 
-    # Extract the file URL from the HTML page
-    file_url = extract_file_url(html_file_path)
+    # Create a thread for the HTTP server
+    http_server_thread = threading.Thread(target=start_http_server, args=(directory, port))
+    http_server_thread.start()
+  
+    #wait for the server to start
+    server_started_event.wait()
 
-    if file_url:
-        # Download the file
-        download_file(file_url, destination_folder)
 
-        # Now do the wireframe processing
+    options = Options()
+    options.add_argument("--window-size=1920x1080")
+    options.add_argument("--verbose")
+    #options.add_argument("--headless")
 
-        #Finally upload to bucket
-    else:
-        print("File URL not found in the HTML page.")
+    options.add_experimental_option("prefs", {
+        "download.default_directory": download_dir,
+        "download.prompt_for_download": False,
+        "download.directory_upgrade": True,
+        "safebrowsing_for_trusted_sources_enabled": False,
+        "safebrowsing.enabled": False
+    })
+
+    driver = webdriver.Chrome(options=options)
+    driver.get(http_file_path)
+
+    print()
+
