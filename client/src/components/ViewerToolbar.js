2c96c2806efed905bb09f18ad942f89e64267773
Aiga115 <aiguljhyldyzbekkyzy@gmail.com>
2023-09-29 17:42:26 +0200

    #CFOS-76 viewer toolbar is more dynamic

--- client/src/components/ViewerToolbar.js
+++ client/src/components/ViewerToolbar.js
@@ -3,12 +3,52 @@ import { IconButton, Stack, Tooltip } from "@mui/material";
 import { KeyboardArrowDownIcon, KeyboardArrowUpIcon, HomeIcon, ZoomInIcon, ZoomOutIcon, TonalityIcon, AutoModeIcon } from '../icons';
 import vars from '../theme/variables';
 
-const options = ['Previous slice', 'Center stack', 'Next slice', 'Auto scroll through slices', 'Zoom in', 'Zoom out', 'Switch to wireframe']
+const options = [
+    {
+        title: "Previous slice",
+        Icon: <KeyboardArrowUpIcon />,
+        onClickFunc: () => console.log("Previous slice"),
+        isVisible: true
+    },
+    {
+        title: "Center stack",
+        Icon: <HomeIcon />,
+        onClickFunc: () => console.log("Center stack"),
+        isVisible: true
+    },
+    {
+        title: "Next slice",
+        Icon: <KeyboardArrowDownIcon />,
+        onClickFunc: () => console.log("Next slice"),
+        isVisible: true
+    },
+    {
+        title: "Auto scroll through slices",
+        Icon: <AutoModeIcon />,
+        onClickFunc: () => console.log("Auto scroll through slices"),
+        isVisible: true
+    },
+    {
+        title: "Zoom in",
+        Icon: <ZoomInIcon />,
+        onClickFunc: () => console.log("Zoom in"),
+        isVisible: true
+    },
+    {
+        title: "Zoom out",
+        Icon: <ZoomOutIcon />,
+        onClickFunc: () => console.log("Zoom out"),
+        isVisible: true
+    },
+    {
+        title: "Switch to wireframe",
+        Icon: <TonalityIcon />,
+        onClickFunc: () => console.log("Switch to wireframe"),
+        isVisible: true
+    }
+]
 export const ViewerToolbar = () => {
 
-    const handleClick = (id) => {
-        console.log("id: ", options[id])
-    }
     return (
         <Stack spacing={0.5} sx={{
             '& .MuiIconButton-root': {
@@ -19,45 +59,19 @@ export const ViewerToolbar = () => {
                 }
             },
             '& .MuiSvgIcon-root': {
-                width: 'auto', 
+                width: 'auto',
                 height: 'auto'
             }
         }}>
-            <Tooltip title="Previous slice" placement="right">
-                <IconButton onClick={() => handleClick(0)}>
-                    <KeyboardArrowUpIcon fontSize="inherit" />
-                </IconButton>
-            </Tooltip>
-            <Tooltip title="Center stack" placement="right">
-                <IconButton onClick={() => handleClick(1)}>
-                    <HomeIcon fontSize="inherit" />
-                </IconButton>
-            </Tooltip>
-            <Tooltip title="Next slice" placement="right">
-                <IconButton onClick={() => handleClick(2)}>
-                    <KeyboardArrowDownIcon fontSize="inherit" />
-                </IconButton>
-            </Tooltip>
-            <Tooltip title="Auto scroll through slices" placement="right">
-                <IconButton onClick={() => handleClick(3)}>
-                    <AutoModeIcon fontSize="inherit" />
-                </IconButton>
-            </Tooltip>
-            <Tooltip title="Zoom in" placement="right">
-                <IconButton onClick={() => handleClick(4)}>
-                    <ZoomInIcon fontSize="inherit" />
-                </IconButton>
-            </Tooltip>
-            <Tooltip title="Zoom out" placement="right">
-                <IconButton onClick={() => handleClick(5)}>
-                    <ZoomOutIcon fontSize="inherit" />
-                </IconButton>
-            </Tooltip>
-            <Tooltip title="Switch to wireframe" placement="right">
-                <IconButton onClick={() => handleClick(6)}>
-                    <TonalityIcon fontSize="inherit" />
-                </IconButton>
-            </Tooltip>
+            {
+                options.map((option) => option.isVisible && (
+                    <Tooltip key={option.title} title={option.title} placement="right">
+                        <IconButton onClick={option.onClickFunc}>
+                            {option.Icon}
+                        </IconButton>
+                    </Tooltip>
+                ))
+            }
         </Stack>
     );
 }
