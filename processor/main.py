357edd72794ae5033e5644b3fd05dd021a3ace31
enicolasgomez <enicolasgomez@gmail.com>
2023-11-02 17:59:58 -0300

    Changed folders back to specification

--- processor/main.py
+++ processor/main.py
@@ -36,7 +36,7 @@ output_directory = os.path.join(web_directory, output_folder)  # Full path to th
 os.makedirs(output_directory, exist_ok=True)
 os.makedirs(download_dir, exist_ok=True)
 
-sub_folders = ["_Atlas", "_ActivityMap"]
+sub_folders = ["Atlas", "ActivityMap"]
 
 def wait_for_file(filename, directory_path, timeout_seconds=300):
     """
