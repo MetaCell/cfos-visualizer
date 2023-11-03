9e546e356a96e0adc5b54f87ae5f1c324bc02625
enicolasgomez <enicolasgomez@gmail.com>
2023-11-03 12:02:17 -0300

    Fixed header on nifti file

--- processor/helpers/wireframe.py
+++ processor/helpers/wireframe.py
@@ -13,14 +13,25 @@ def process_nifti_file(nifti_file_location, target_file_location):
     # Get the data as float32 to preserve the original data type
     data = img.get_fdata(dtype=np.float32)
 
-    def transform_slice(data_slice):
-        # Apply Sobel filter to find edges
-        edge_sobel_x = ndimage.sobel(data_slice, axis=0)
-        edge_sobel_y = ndimage.sobel(data_slice, axis=1)
-        # Combine the two edges to get the final edge magnitude
-        edges = np.hypot(edge_sobel_x, edge_sobel_y)
+    def transform_slice(data_slice, sobel_filter=True):
+        if sobel_filter:
+            # Apply Sobel filter to find edges
+            edge_sobel_x = ndimage.sobel(data_slice, axis=0)
+            edge_sobel_y = ndimage.sobel(data_slice, axis=1)
+            # Combine the two edges to get the final edge magnitude
+            edges = np.hypot(edge_sobel_x, edge_sobel_y)
 
-        return edges.astype(np.float32)  # Make sure to cast back to float32 if needed
+            return edges.astype(np.float32)  # Make sure to cast back to float32 if needed
+        else:
+            # Apply a Gaussian filter directly on the slice without changing its data type
+            blurred_slice = ndimage.gaussian_filter(data_slice, sigma=1)
+
+            # It's not recommended to convert to UINT8 if you want to preserve the original data type,
+            # but for Canny edge detection it is a necessary step (which is a lossy process).
+            # You need to decide whether you really want to do this.
+            edges = cv2.Canny(blurred_slice.astype(np.uint8), 250, 250)
+
+            return edges
 
     transformed_slices = []
 
@@ -34,7 +45,7 @@ def process_nifti_file(nifti_file_location, target_file_location):
     # Stack the transformed slices back into a 3D array
     transformed_data = np.stack(transformed_slices, axis=-1)
     # Create a new NIfTI image with the transformed data
-    transformed_img = nib.Nifti1Image(transformed_data, img.affine)
+    transformed_img = nib.Nifti1Image(transformed_data, img.affine, img.header)
     # Save the transformed NIfTI image to a different file
     nib.save(transformed_img, target_file_location)
 
