a829052e57a980dc4f69ba35c4545c09e2d33d7b
salam dalloul <salamdalloul93@gmail.com>
2024-04-11 17:39:14 +0200

    add alert component

---
+++ client/src/components/MetaDataViewerComponents/CustomAlert.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import {Alert, Button, Collapse, IconButton, Stack, Typography} from "@mui/material";
+import CloseIcon from '@mui/icons-material/Close';
+import variables from "../../theme/variables";
+
+const {gray100, gray300, gray700, gray800, gray400} = variables
+export const CustomAlert = () =>
+{
+  const [open, setOpen] = React.useState(true);
+  
+  return (
+    <Collapse in={open}>
+      <Alert
+        action={
+          <IconButton
+            aria-label="close"
+            color="inherit"
+            size="small"
+            onClick={() => {
+              setOpen(false);
+            }}
+          >
+            <CloseIcon
+              sx={{
+                color: gray400,
+                fontSize: '1.25rem'
+            }} />
+          </IconButton>
+        }
+        icon={false}
+        sx={{
+          borderRadius: '0.75rem',
+          border: `1px solid ${gray700}`,
+          background: gray800,
+          padding: '1rem'
+        }}
+      >
+        <Stack spacing={'.75rem'}>
+          <Typography variant='h4' fontWeight={400} color={gray300}>The selected atlas is associated with this experiment.</Typography>
+          <Button variant='text' fullWidth={false} sx={{
+            '&.MuiButton-text': {
+              padding: '0',
+              width: 'fit-content',
+              textTransform: 'none',
+            }
+          }}>
+            <Typography variant='h4' fontWeight={600} color={gray100}>See other associated experiments</Typography>
+          </Button>
+        </Stack>
+      </Alert>
+    </Collapse>
+  )
+};
--- client/src/components/MetadataViewer.js
+++ client/src/components/MetadataViewer.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import {Alert, Collapse, IconButton, Stack, Typography} from "@mui/material";
-import CloseIcon from '@mui/icons-material/Close';
-import variables from "../theme/variables";
+import { Stack } from "@mui/material";
+import {CustomAlert} from "./MetaDataViewerComponents/CustomAlert";
 
-const {gray100, gray300, gray700, gray800} = variables
 export const MetadataViewer = () =>
 {
-  const [open, setOpen] = React.useState(true);
-
   return (
     <>
       <Stack
@@ -20,34 +16,7 @@ export const MetadataViewer = () =>
          width: '100%',
          padding: '1.5rem'
        }}>
-        <Collapse in={open}>
-          <Alert
-            action={
-              <IconButton
-                aria-label="close"
-                color="inherit"
-                size="small"
-                onClick={() => {
-                  setOpen(false);
-                }}
-              >
-                <CloseIcon fontSize="inherit" />
-              </IconButton>
-            }
-            icon={false}
-            sx={{
-              borderRadius: '0.75rem',
-              border: `1px solid ${gray700}`,
-              background: gray800,
-              padding: '1rem'
-            }}
-          >
-            <Stack spacing={'.75rem'}>
-              <Typography variant='h4' fontWeight={400} color={gray300}>The selected atlas is associated with this experiment.</Typography>
-              <Typography variant='h4' fontWeight={600} color={gray100}>See other associated experiments</Typography>
-            </Stack>
-          </Alert>
-        </Collapse>
+        <CustomAlert />
       </Stack>
     </>
   )
