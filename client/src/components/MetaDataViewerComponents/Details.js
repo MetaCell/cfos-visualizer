888fe96d51cdb23968ce5feee71d5c9d42638a40
salam dalloul <salamdalloul93@gmail.com>
2024-05-02 16:15:14 +0300

    view empty placeholder

--- client/src/components/MetaDataViewerComponents/Details.js
+++ client/src/components/MetaDataViewerComponents/Details.js
@@ -1,7 +1,11 @@
-import {Stack} from "@mui/material";
+import {Box, Stack, Typography} from "@mui/material";
 import React from "react";
 import Detail from "./Detail";
 import {Publications} from "./Publications";
+import variables from "../../theme/variables";
+
+const { gray100 } = variables
+
 export const Details = ({experiment}) => {
   const renderComponent = (key, value) => {
     if (typeof value === 'string') {
@@ -15,11 +19,19 @@ export const Details = ({experiment}) => {
   const renderDetails = () => {
     return experiment && Object.entries(experiment).map(([key, value]) => (
       <React.Fragment key={key}>
+        <Stack spacing='1.5rem'>
         {renderComponent(key, value)}
+        </Stack>
       </React.Fragment>
     ));
   };
-  return <Stack spacing='1.5rem'>
-    {renderDetails()}
-  </Stack>
+  return <>
+    {
+      experiment ? renderDetails() : <Box textAlign='center' mt='15%'>
+        <Typography variant='h4' fontWeight={600} color={gray100}> No metadata for this experiment</Typography>
+      </Box>
+    }
+  </>
+  
+  
 }
